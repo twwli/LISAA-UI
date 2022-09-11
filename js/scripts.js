1e0bb214570a011cea4b0770364563f836ba83f7
twwli <hello@theworldwelivein.co.uk>
2022-09-11 15:33:09 +0200

    Remove opacity on header and footer

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,9 @@ var accordion = $('.accordion');
 accordion.mouseenter(function(){
   $(this).addClass('active-acc');
   accordion.not(this).addClass('not-acc');
-  $('header, footer').addClass('not-acc');
 }).mouseleave(function(){
    $(this).removeClass('active-acc');
    accordion.not(this).removeClass('not-acc');
-   $('header, footer').removeClass('not-acc');
 });
 
 var sectionTrigger = $('.section-trigger');
